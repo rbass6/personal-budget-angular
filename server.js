52b861f05431f620b0550beb4befa2bdebefa678
Ryan <rbass6@uncc.edu>
2023-09-23 17:36:24 -0400

    Add cors

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const budgetData = require('./budget-data.json')
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'))
+//app.use('/', express.static('public'))
+app.use(  cors({
+  allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+  exposedHeaders: ["authorization"], // you can change the headers
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false
+}))
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
